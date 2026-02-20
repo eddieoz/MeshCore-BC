bb941e9df887e2edd34d4db3f2673024b3e3f264
eddieoz <eddieoz@pm.me>
2026-02-20 22:11:06 +0200

    feat: Add quintuple press event detection and callback functionality for buttons.

--- examples/companion_radio/AbstractUITask.h
+++ examples/companion_radio/AbstractUITask.h
@@ -1,34 +1,27 @@
 #pragma once
 
+#include <Arduino.h>
 #include <MeshCore.h>
+#include <helpers/BaseSerialInterface.h>
+#include <helpers/SensorManager.h>
 #include <helpers/ui/DisplayDriver.h>
 #include <helpers/ui/UIScreen.h>
-#include <helpers/SensorManager.h>
-#include <helpers/BaseSerialInterface.h>
-#include <Arduino.h>
 
 #ifdef PIN_BUZZER
-  #include <helpers/ui/buzzer.h>
+#include <helpers/ui/buzzer.h>
 #endif
 
 #include "NodePrefs.h"
 
-enum class UIEventType {
-    none,
-    contactMessage,
-    channelMessage,
-    roomMessage,
-    newContactMessage,
-    ack
-};
+enum class UIEventType { none, contactMessage, channelMessage, roomMessage, newContactMessage, ack };
 
 class AbstractUITask {
 protected:
-  mesh::MainBoard* _board;
-  BaseSerialInterface* _serial;
+  mesh::MainBoard *_board;
+  BaseSerialInterface *_serial;
   bool _connected;
 
-  AbstractUITask(mesh::MainBoard* board, BaseSerialInterface* serial) : _board(board), _serial(serial) {
+  AbstractUITask(mesh::MainBoard *board, BaseSerialInterface *serial) : _board(board), _serial(serial) {
     _connected = false;
   }
 
@@ -39,8 +32,9 @@ public:
   bool isSerialEnabled() const { return _serial->isEnabled(); }
   void enableSerial() { _serial->enable(); }
   void disableSerial() { _serial->disable(); }
+  BaseSerialInterface *getSerial() { return _serial; }
   virtual void msgRead(int msgcount) = 0;
-  virtual void newMsg(uint8_t path_len, const char* from_name, const char* text, int msgcount) = 0;
+  virtual void newMsg(uint8_t path_len, const char *from_name, const char *text, int msgcount) = 0;
   virtual void notify(UIEventType t = UIEventType::none) = 0;
   virtual void loop() = 0;
 };
